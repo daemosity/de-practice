ed7eaf673c16603d35384720c97df19e79fd06e1
daemosity <evanescentink@gmail.com>
2023-11-26 18:26:36 -0600

    refactoring with pyarrow and updating files to keep data link updated

--- ingest_data.py
+++ ingest_data.py
@@ -9,6 +9,7 @@ import os
 import argparse
 from time import time
 import pandas as pd
+import pyarrow.parquet as pq
 from sqlalchemy import create_engine
 
 def main(params):
@@ -28,14 +29,11 @@ def main(params):
 
     # read in ny_yellow_taxi data from 2021, adjust datatypes for pandas to detect proper DLL format
     # setting up file to read file into dataframe one chunk at a time
-    df_iter = pd.read_parquet(parquet_name, iterator=True, chunksize=100000)
-
-    # take first batch and correct datatypes
-    df = next(df_iter)
-
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-    df.passenger_count = (df.passenger_count).fillna(0).astype(int)
+    parquet_file = pq.ParquetFile(parquet_name)
+    batch_iter = parquet_file.iter_batches(100000)
+    
+    # take first batch
+    df = next(batch_iter).to_pandas()
 
     # create table using just the dataFrame headers
     df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
@@ -47,11 +45,7 @@ def main(params):
     while len(df) == 100000:
         t_start = time()
         
-        df = next(df_iter)
-        
-        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
-        df.passenger_count = (df.passenger_count).fillna(0).astype(int)
+        df = next(batch_iter).to_pandas()
 
         df.to_sql(name=table_name, con=engine, if_exists='append')
 
@@ -66,13 +60,13 @@ if __name__ == '__main__':
 
     # user, password, host, port, database name, table name
     # url of the csv
-    parser.add_argument('user', help='username for postgres')
-    parser.add_argument('password', help='password for postgres')
-    parser.add_argument('host', help='host for postgres')
-    parser.add_argument('port', help='port for postgres')
-    parser.add_argument('db', help='database name for postgres')
-    parser.add_argument('table-name', help='name of the table where we will write the results to')
-    parser.add_argument('url', help='url of the csv file')
+    parser.add_argument('--user', help='username for postgres')
+    parser.add_argument('--password', help='password for postgres')
+    parser.add_argument('--host', help='host for postgres')
+    parser.add_argument('--port', help='port for postgres')
+    parser.add_argument('--db', help='database name for postgres')
+    parser.add_argument('--table_name', help='name of the table where we will write the results to')
+    parser.add_argument('--url', help='url of the csv file')
 
     args = parser.parse_args()
     main(args)
